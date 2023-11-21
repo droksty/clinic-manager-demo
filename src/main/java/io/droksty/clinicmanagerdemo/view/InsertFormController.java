cabce383323d7caac3e5fc1cf0ecaf2d025a1ffa
droksty <droksty@gmail.com>
2023-11-21 09:47:53 +0200

    add view layer, insert-form.fxml, InsertFormController class, update module-info

---
+++ src/main/java/io/droksty/clinicmanagerdemo/view/InsertFormController.java
@@ -0,0 +1,41 @@
+package io.droksty.clinicmanagerdemo.view;
+
+
+import io.droksty.clinicmanagerdemo.controller.Controller;
+import io.droksty.clinicmanagerdemo.dto.PatientDTOFactory;
+import io.droksty.clinicmanagerdemo.dto.PatientDTO;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class InsertFormController {
+    private final Controller controller = new Controller();
+
+    @FXML
+    private TextField citizenIdField;
+    @FXML
+    private TextField lastnameField;
+    @FXML
+    private TextField firstnameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField phoneField;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private Button backButton;
+
+
+    @FXML
+    private void onFormSubmit() {
+        PatientDTO dto = PatientDTOFactory.createFromUserInput(
+                citizenIdField.getText(),
+                lastnameField.getText(),
+                firstnameField.getText(),
+                emailField.getText(),
+                phoneField.getText()
+        );
+        controller.doInsert(dto);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,9 @@ module io.droksty.clinicmanagerdemo {
     requires javafx.fxml;
     requires java.sql;
     requires commons.dbcp2;
-
+    requires java.management;
 
     opens io.droksty.clinicmanagerdemo to javafx.fxml;
+    opens io.droksty.clinicmanagerdemo.view to javafx.fxml;
     exports io.droksty.clinicmanagerdemo;
 }
